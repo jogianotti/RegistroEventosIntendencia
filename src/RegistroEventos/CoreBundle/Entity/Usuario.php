5b3c5ca4d0bc2cd84d85e2d2b2ae29a414d9c26c
bitora <bitora@gmail.com>
2014-10-23 17:44:58 -0300

    Se crearon las clases usuario y evento
    Clases del esquema de bases de datos

--- src/RegistroEventos/CoreBundle/Entity/Usuario.php
+++ src/RegistroEventos/CoreBundle/Entity/Usuario.php
@@ -1,39 +1,103 @@
 <?php
 
-// src/RegistroEventos/CoreBundle/Entity/Usuario.php
-
 namespace RegistroEventos\CoreBundle\Entity;
 
-use FOS\UserBundle\Model\User as UsuarioBase;
 use Doctrine\ORM\Mapping as ORM;
-
+use FOS\UserBundle\Model\User as UsuarioBase;
 /**
- * @ORM\Entity
- * @ORM\Table(name="Usuarios")
+ * Usuario
+ *
+ * @ORM\Table(name="usuarios")
+ * @ORM\Entity(repositoryClass="RegistroEventos\CoreBundle\Entity\UsuarioRepository")
  */
 class Usuario extends UsuarioBase
 {
-
     /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=80)
+     */
+    private $nombre;
+
+    
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="baja", type="boolean")
+     */
+    private $baja;
+    
+
+    /**
+     * @ORM\OneToMany(targetEntity="Evento", mappedBy="usuario")
+     **/
+    protected $eventos;
 
     /**
      * Get id
      *
-     * @return integer
+     * @return integer 
      */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    
+        
+    /**
+     * Set baja
+     *
+     * @param boolean $baja
+     * @return Usuario
+     */
+    public function setBaja($baja)
+    {
+        $this->baja = $baja;
+
+        return $this;
+    }
+
+    /**
+     * Get baja
+     *
+     * @return boolean 
+     */
+    public function getBaja()
+    {
+        return $this->baja;
+    }
 }
