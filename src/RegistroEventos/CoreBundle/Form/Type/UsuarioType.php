243f6e4f3fb344034dc645c30dc970f5ad733071
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2014-11-13 00:44:51 -0300

    Se modifico la configuración de seguridad y acceso. Se agrego el controlador de la administración y se reubicaron los repositorios.

--- src/RegistroEventos/CoreBundle/Form/Type/UsuarioType.php
+++ src/RegistroEventos/CoreBundle/Form/Type/UsuarioType.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace RegistroEventos\CoreBundle\Form;
+namespace RegistroEventos\CoreBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList;
 
 class UsuarioType extends AbstractType
 {
@@ -15,8 +16,19 @@ class UsuarioType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('username')
+            ->add('email')
+            ->add('password')
+            ->add('roles', 'choice', array(
+                'choice_list' => new ChoiceList(
+                                        array('ROLE_ADMINISTRADOR','ROLE_SUPERVISOR','ROLE_INTENDENTE'),
+                                        array( 'Administrador','Supervisor','Intendente')
+                                    ),
+                'required'  => true,
+                'multiple' => false,
+                
+            ))
             ->add('nombre')
-            ->add('baja')
         ;
     }
     
